063029a5d165ba6c88077e3ef490004376b07d14
Jeremy Wayland <47305172+jeremy-wayland@users.noreply.github.com>
2026-06-18 12:51:29 +0200

    chore: bump version to 0.1.3 and fix release history in CHANGELOG (#12)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "Trailed"
 author = "Krv Labs"
 copyright = "2026, Krv Labs"
-release = "0.1.0"
+release = "0.1.3"
 
 extensions = [
     "sphinx.ext.autodoc",
